38e7772e7551d8e2f1e647eb5ddd1b85352f1727
Kozlov Arseny <jetpilotat1il@gmail.com>
2025-03-03 22:52:15 +0300

    add simple home page

--- lib/main.dart
+++ lib/main.dart
@@ -6,16 +6,11 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Tink Offer: Business Assistant',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      debugShowCheckedModeBanner: false,
       home: const MyHomePage(),
     );
   }
@@ -32,11 +27,20 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.amber,
+        title: Text('Home page title'),
+      ),
       body: SafeArea(
-        child: Column(
-          children: [
-            Text('hello world')
-          ],
+        
+        child: Container(
+          width: double.infinity,
+          child: Column(
+            children: [
+              Text('hello world')
+            ],
+          ),
         ),
       ),
     );
